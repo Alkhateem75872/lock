0065c6937d4b060c08c72518841b084384f94162
Muhammad <mohamedkam000@gmail.com>
2025-08-11 15:24:21 +0200

    test build 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ android {
 }
 
 dependencies {
-    implementation(project(":appintro"))
+//    implementation(project(":appintro"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
