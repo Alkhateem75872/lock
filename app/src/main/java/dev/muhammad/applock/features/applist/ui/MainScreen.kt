36dbf4aedc9af732647e6f437ffbfb8f9e4e435d
Muhammad <mohamedkam000@gmail.com>
2025-08-11 20:13:08 +0200

    this thing better build

--- app/src/main/java/dev/muhammad/applock/features/applist/ui/MainScreen.kt
+++ app/src/main/java/dev/muhammad/applock/features/applist/ui/MainScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
+//import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.HorizontalDivider
